c4b79a7bc08a21f77a7986ff674f8cdd941aeb7b
Rahul Katiyar <rahulkatiyar19955@gmail.com>
2020-04-10 18:56:36 +0530

    deleting and updation of Book Added

--- project/routes/Routes.py
+++ project/routes/Routes.py
@@ -8,7 +8,7 @@ from project.models.books import BooksModel
 
 @app.route('/')
 def index():
-    return "hello"
+    return jsonify({'message': "The API is working : Root Endpoint"}), 200
 
 
 @app.before_first_request
@@ -25,30 +25,32 @@ def login():
             user_json = dict()
             user_json['username'] = user.username
             user_json['id'] = user.id
-            access_token = create_access_token(identity=user_json)
+            access_token = create_access_token(identity=user_json, fresh=True)
             refresh_token = create_refresh_token(identity=user_json)
             return jsonify({'message': 'Login Successful',
                             'access_token': access_token,
                             'refresh_token': refresh_token})
-    return "Login Unsuccessful"
+    return jsonify({'message': "Login Unsuccessful"}), 401
 
 
 @app.route('/reauthenticate', methods=['POST'])
 @jwt_refresh_token_required
 def reauthenticate():
     curr_user = get_jwt_identity()
-    access_token = create_access_token(identity=curr_user)
+    access_token = create_access_token(identity=curr_user, fresh=False)
     return jsonify({'message': 'Successful Creating of access token',
-                    'access_token': access_token})
+                    'access_token': access_token}), 200
 
 
 @app.route('/register', methods=['POST'])
 def create_user():
     data_received = request.get_json()
-    # todo check username exist or not before adding user to database
     new_user = UserModel(data_received['username'], data_received['password'])
+    if new_user.user_exist():
+        return jsonify({'message': "Username already exist"}), 400
     new_user.save_to_db()
-    return "creating " + " " + data_received['username'] + " " + data_received['password']
+    return jsonify({'message': "Created Successfully",
+                    "username":data_received['username']}), 200
 
 
 @app.route('/favourite_book', methods=['POST'])
@@ -59,43 +61,68 @@ def adding_book():
     amazon_url = data_received['amazon_url']
     author = data_received['author']
     genre = data_received['genre']
-    # add check if it is already exist or not
     curr_user = get_jwt_identity()
-    print(curr_user)
-    try:
-        new_book = BooksModel(title, amazon_url, author, genre, curr_user['id'])
-        new_book.save_to_db()
+    new_book = BooksModel(title, amazon_url, author, genre, curr_user['id'])
+    if BooksModel.find_by_name(title) is not None:
+        return jsonify({'message': "Book with same title already exist"}), 400
+    else:
+        try:
+            new_book.save_to_db()
+        except:
+            return jsonify({'message': "error occured"}), 400
 
-    except:
-        return "error occured"
-
-    return "added: " + title
+    return jsonify({'message': "added successful",
+                    "title":title}), 200
 
 
 @app.route('/favourite_book', methods=['GET'])
 @jwt_required
 def list_book():
-    # data_received = request.get_json()
     curr_user = get_jwt_identity()
     books = BooksModel.find_by_user_id(curr_user['id'])
     list_of_books = []
     for book in books:
         temp_book = {}
+        temp_book['book_id'] = book.id
         temp_book['title'] = book.title
         temp_book['amazon_url'] = book.amazon_url
         temp_book['author'] = book.author
         temp_book['genre'] = book.genre
         list_of_books.append(temp_book)
-    return jsonify({'list_of_books': list_of_books})
+    return jsonify({'list_of_books': list_of_books}), 200
 
 
 @app.route('/favourite_book', methods=['PUT'])
 @fresh_jwt_required
 def update_book():
-    return "update_book"
+    data_received = request.get_json()
+    book_id = data_received['book_id']
+    curr_user = get_jwt_identity()
+    temp_book = BooksModel.find_by_book_id(book_id)
+    if temp_book is not None and temp_book.user_id == curr_user['id']:
+        temp_book.title = data_received['title']
+        temp_book.amazon_url = data_received['amazon_url']
+        temp_book.author = data_received['author']
+        temp_book.genre = data_received['genre']
+        temp_book.save_to_db()
+        return jsonify({'message': "update book successful"}), 200
+    else:
+        return jsonify({'message': "Book with the Given Id is not found"}), 404
 
 
 @app.route('/favourite_book', methods=['DELETE'])
 @fresh_jwt_required
 def delete_book():
-    return "delete_book"
+    data_received = request.get_json()
+    book_id = data_received['book_id']
+    curr_user = get_jwt_identity()
+    try:
+        temp_book = BooksModel.find_by_book_id(book_id)
+        if temp_book is not None and temp_book.user_id == curr_user['id']:
+            BooksModel.delete_by_book_user_id(book_id, curr_user['id'])
+            db.session.commit()
+            return jsonify({'message': "deletion of book Successful"}), 200
+        else:
+            return jsonify({'message': "No book found with the given Book_Id"}), 404
+    except:
+        return jsonify({'message': "deletion of book Unsuccessful"}), 404
